e573437b3be5399753219404c8db48673bb831ce
LissethReinoso <LissethReinoso30@users.noreply.github.com>
2020-05-12 21:07:54 -0500

    Completar paquetes y clases 2

--- src/ec/edu/ups/controlador/ControladorUsuario.java
+++ src/ec/edu/ups/controlador/ControladorUsuario.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.ups.controlador;
 
 /**
  *
  * @author Usuario
  */
+import ec.edu.ups.idao.IUsuarioDao;
+import ec.edu.ups.modelo.Usuario;
+import ec.ups.edu.dao.UsuarioDao;
+import ec.ups.edu.vista.VistaUsuario;
+import java.util.List;
+
+
 public class ControladorUsuario {
+    //Objetos vista
+    private VistaUsuario vistaUsuario;
+    private Usuario usuario;
+    private IUsuarioDao usuarioDao;
     
+    //constructor
+    public ControladorUsuario(VistaUsuario vistaUsuario){
+        this.vistaUsuario=vistaUsuario;
+        this.usuarioDao= new usuarioDao();
+    }
+    
+    //llamar al DAO para llamar al usuario
+    public void registrar(){
+        usuario=vistaUsuario.agregarUsuario();
+        usuarioDao.create(usuario);    
+    }
+    
+    //llama al DAO para obtener un usuario por la cédula y luego muestra en la vista.
+    public void verUsuario(){
+        int cedula= vistaUsuario.buscarUsuario();
+        usuario=usuarioDao.read(cedula);
+        vistaUsuario.verUsuario(usuario);
+    }
+    //llama al DAO para actualizar al usuario
+    public void 
+            
 }
--- src/ec/ups/edu/vista/VistaUsuario.java
+++ src/ec/ups/edu/vista/VistaUsuario.java
@@ -5,6 +5,61 @@ package ec.ups.edu.vista;
  *
  * @author Lisseth Reinoso
  */
+import ec.edu.ups.modelo.*;
+import java.util.List;
+import java.util.Scanner;
+
 public class VistaUsuario {
+      private Scanner leer;
+
+    public VistaUsuario() {
+        leer=new Scanner(System.in);
+    }
     
+      public Usuario agregarUsuario(){
+          leer=new Scanner(System.in);
+          System.out.println("Ingrese sus datos: ");
+          int cedula=leer.nextInt();
+          String nombre=leer.nextLine();
+          String apellido=leer.nextLine();
+          String correo=leer.nextLine();
+          String contraseña=leer.nextLine();
+          return new Usuario(cedula,nombre,apellido,correo,contraseña);
+      }
+      
+      public Usuario modificarUsuario(){
+          leer=new Scanner(System.in);
+          System.out.println("Ingrese su cédula para modificar sus datos: ");
+          int cedula=leer.nextInt();
+          System.out.println("Ingrese los datos: ");
+          String nombre=leer.nextLine();
+          String apellido=leer.nextLine();
+          String correo=leer.nextLine();
+          String contraseña=leer.nextLine();
+          return new Usuario(cedula,nombre,apellido,correo,contraseña);
+      }
+      public Usuario eliminarUsuario(){
+          leer=new Scanner(System.in);
+          System.out.println("Ingrese la cédula: ");
+          int cedula=leer.nextInt();
+          return new Usuario(cedula,null,null);
+      }
+      public int buscarUsuario(){
+          leer=new Scanner(System.in);
+          System.out.println("Ingrese el usuario a buscar");
+          int cedula=leer.nextInt();
+          return cedula;
+      }
+      
+      public void verUsuario(Usuario usuario){
+          System.out.println("USUARIO: "+usuario);
+      }
+      
+      public void verUsuarios(List<Usuario> usuarios){
+          for(Usuario usuario:usuarios){
+              System.out.println("DATOS DE LOS USUARIOS: "+usuario);
+          }
+      }
+      
+     
 }
